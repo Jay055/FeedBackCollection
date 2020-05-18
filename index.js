9ba62f5914c982787ddecd8a612a0973871320c1
jay055 <nwachukwuuche@gmail.com>
2020-05-18 17:09:26 +0200

    Added Start Script

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const express = require('express');
 // Express application
 const app = express(); 
 
-
+// route handler associated with the get method 
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
 })
 
+// Set port with Environment Variables 
+const PORT = process.env.PORT || 5000; 
 
+// Node listens to app 5000 
 app.listen(5000);
