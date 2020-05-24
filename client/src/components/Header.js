25a0f062ee9abd7c9ecc58c54efd58bfc8def7d2
jay055 <nwachukwuuche@gmail.com>
2020-05-24 09:54:20 +0200

    Webpack css, introduction to materialize

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+     
+  <nav>
+  <div className="nav-wrapper">
+    <a href="#" className="brand-logo">Feedback</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+    
+      <li><a href="badges.html">Login with Googles</a></li>
+    
+    </ul>
+  </div>
+</nav>
+      
     );
   }
 }
