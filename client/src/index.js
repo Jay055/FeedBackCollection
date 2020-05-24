c9c0062fb7bc9ab8e7847b826ebe8385ab4b15ef
jay055 <nwachukwuuche@gmail.com>
2020-05-24 07:52:34 +0200

    Added Auth Reducer

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reducers from './components/reducers';
 
 import App from './components/App';
 
 // Set up Redux Store 
-const store =  createStore(() => [], {}, applyMiddleware())
+const store =  createStore(reducers, {}, applyMiddleware())
 
 
 
 
 
 
+// Nest App to Provider (store)
 ReactDOM.render(
 <Provider store={store}><App /> </Provider>, document.querySelector('#root'));
